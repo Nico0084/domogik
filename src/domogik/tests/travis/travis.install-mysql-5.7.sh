ed647745df2f130c95e0b8d2954ebd7b149e1b8c
Nico0084 <nico84dev@gmail.com>
2016-07-17 01:55:01 +0200

    Travis, change dist for travis.install-mysql-5.7.sh

--- src/domogik/tests/travis/travis.install-mysql-5.7.sh
+++ src/domogik/tests/travis/travis.install-mysql-5.7.sh
@@ -2,7 +2,7 @@
 # The -e option will make the bash stop if any command raise an error ($? != 0)
 
 echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7-dmr | sudo debconf-set-selections
-wget http://dev.mysql.com/get/mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
-sudo dpkg --install mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
+wget http://cdn.mysql.com//Downloads/MySQL-5.7/mysql-common_5.7.13-1ubuntu14.04_amd64.deb
+sudo dpkg --install mysql-common_5.7.13-1ubuntu14.04_amd64.deb
 sudo apt-get update -q
 sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server
