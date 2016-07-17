f0876079d664797f7bba0d9c06ef1f76c222753c
Nico0084 <nico84dev@gmail.com>
2016-07-17 23:54:07 +0200

    Travis, try purge mysql, force vers

--- src/domogik/tests/travis/travis.install-mysql-5.7.sh
+++ src/domogik/tests/travis/travis.install-mysql-5.7.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 # The -e option will make the bash stop if any command raise an error ($? != 0)
 
+sudo apt-get purge mysql
 echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7-dmr | sudo debconf-set-selections
 wget http://cdn.mysql.com//Downloads/MySQL-5.7/mysql-common_5.7.13-1ubuntu14.04_amd64.deb
 sudo dpkg --install mysql-common_5.7.13-1ubuntu14.04_amd64.deb
 sudo apt-get update -q
-sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server
+sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server=5.7.13
