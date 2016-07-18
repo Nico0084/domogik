d24e8c7b89c3f6b04fc586a1d3acc5a06874e06a
Nico0084 <nico84dev@gmail.com>
2016-07-18 18:16:58 +0200

    Travis, try mysql indtall from dpkg

--- src/domogik/tests/travis/travis.install-mysql-5.7.sh
+++ src/domogik/tests/travis/travis.install-mysql-5.7.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 # The -e option will make the bash stop if any command raise an error ($? != 0)
 
-wget http://dev.mysql.com/get/mysql-apt-config_0.6.0-1_all.deb
-sudo dpkg -i mysql-apt-config_0.6.0-1_all.deb
-sudo apt-get update -q
-sudo apt-get install -q -y mysql-server
+echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7-dmr | sudo debconf-set-selections
+wget http://cdn.mysql.com//Downloads/MySQL-5.7/mysql-common_5.7.13-1ubuntu14.04_amd64.deb
+sudo dpkg --install mysql-common_5.7.13-1ubuntu14.04_amd64.deb
+sudo dpkg -i -y mysql-apt-config_5.7.13-1_all.deb
+#sudo apt-get update -q
+#sudo apt-get install -y -o Dpkg::Options::=--force-confnew mysql-server=5.7.13
