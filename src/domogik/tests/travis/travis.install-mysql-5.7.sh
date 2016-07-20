29a581b5e763109e6a0d3991f09589768e0f6037
Nico0084 <nico84dev@gmail.com>
2016-07-21 00:32:34 +0200

    Travis, fix -v option"

--- src/domogik/tests/travis/travis.install-mysql-5.7.sh
+++ src/domogik/tests/travis/travis.install-mysql-5.7.sh
@@ -3,7 +3,7 @@
 
 echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7-dmr | sudo debconf-set-selections
 wget http://cdn.mysql.com//Downloads/MySQL-5.7/mysql-common_5.7.13-1ubuntu14.04_amd64.deb
-sudo dpkg --install mysql-common_5.7.13-1ubuntu14.04_amd64.deb
-sudo dpkg -i -y mysql-apt-config_5.7.13-1_all.deb
+sudo dpkg -i mysql-common_5.7.13-1ubuntu14.04_amd64.deb
+sudo dpkg -i mysql-apt-config_5.7.13-1_all.deb
 #sudo apt-get update -q
 #sudo apt-get install -y -o Dpkg::Options::=--force-confnew mysql-server=5.7.13
