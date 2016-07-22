2766bb0fad5a515b5517018b47258acf6ba51fde
Nico0084 <nico84dev@gmail.com>
2016-07-22 11:46:29 +0200

    Travis, Remove mysql-client dependency

--- src/domogik/tests/travis/travis-install-dependencies.sh
+++ src/domogik/tests/travis/travis-install-dependencies.sh
@@ -7,7 +7,7 @@ pip install pyzmq
 pip install argparse
 sudo apt-get install -y python2.7-dev gcc
 sudo apt-get install -y libssl-dev
-sudo apt-get install -y libmysqlclient-dev mysql-client
+sudo apt-get install -y libmysqlclient-dev
 pip install psycopg2
 pip install docutils
 #pip install python-daemon==2.0.2
