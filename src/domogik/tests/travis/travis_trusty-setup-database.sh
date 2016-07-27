2e1a4055e39e728f54c5166721cb9af390e63080
Nico0084 <nico84dev@gmail.com>
2016-07-27 20:15:29 +0200

    Travis, travis_trusty-setup-database.sh try add travis user all privileges to domogikdb

--- src/domogik/tests/travis/travis_trusty-setup-database.sh
+++ src/domogik/tests/travis/travis_trusty-setup-database.sh
@@ -2,5 +2,7 @@
 # The -e option will make the bash stop if any command raise an error ($? != 0)
 
 mysql -u root -e 'create database domogik;'
-echo "USE mysql;\nUPDATE user SET password=PASSWORD('domopass') WHERE user='travis';\nFLUSH PRIVILEGES;\n" | mysql -u root
+echo "USE mysql;\nUPDATE user SET password=PASSWORD('domopass') WHERE user='travis@localhost';\nFLUSH PRIVILEGES;\n" | mysql -u root
+echo "USE mysql;\nGRANT ALL PRIVILEGES ON domogik.* TO travis@localhost;\n" | mysql -u root
+
 
